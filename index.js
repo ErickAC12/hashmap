995de187c1f28355cca447cc4de84a5ecbb00ecc
Erick <112605408+Ep1ore@users.noreply.github.com>
2024-06-17 18:47:13 -0600

    set function

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ class HashMap {
         this.buckets = new Array(16).fill(null);
         this.capacity = this.buckets.length;
         this.loadFactor = 0.75;
+        this.stored = 0;
     }
 
     hash(key) {
@@ -20,7 +21,35 @@ class HashMap {
         for (let i = 0; i < key.length; i++) {
             hashCode = primeNumber * hashCode + key.charCodeAt(i);
         }
-        return hashCode % 16;
+        return hashCode % this.buckets.length;
+    }
+
+    set(key, value) {
+        if (this.stored >= this.capacity * this.loadFactor) {
+            // To add increaseBuckets function
+            this.increaseBuckets();
+        }
+        const bucket = this.hash(key);
+        // To add has function
+        if (!this.has(key)) {
+            const newNode = new Node(key, value);
+            if (this.buckets[bucket] === null) {
+                this.stored += 1;
+                this.buckets[bucket] = newNode;
+            } else {
+                let current = this.buckets[bucket];
+                while (current.nextNode !== null) {
+                    current = current.nextNode;
+                }
+                current.nextNode = newNode;
+            }
+        } else {
+            let current = this.buckets[bucket];
+            while (current.key !== key) {
+                current = current.nextNode;
+            }
+            current.value = value;
+        }
     }
 }
 
